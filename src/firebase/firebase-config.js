c58ea573bd1ab58a4fd4f5adbe83fa34ed256268
aegirorn <41763474+aegirorn@users.noreply.github.com>
2022-03-30 17:12:48 +0000

    Firebase initalized

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -9,7 +9,7 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 
-const firebaseConfig = {
+/* const firebaseConfig = {
   apiKey: "AIzaSyC8WRfzb3Nk1rEQRLxLmoxzMJjBU3rae8Q",
   authDomain: "fb-suggestions.firebaseapp.com",
   projectId: "fb-suggestions",
@@ -17,16 +17,16 @@ const firebaseConfig = {
   messagingSenderId: "713127360551",
   appId: "1:713127360551:web:af7e1538fde443f203f602",
 };
-
+ */
 // New Firebase configuration
-/* const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyBNC6tvSX3zPeO6liOiI4PqeKjTpOc86oQ",
   authDomain: "suggestion-app-firebase.firebaseapp.com",
   projectId: "suggestion-app-firebase",
   storageBucket: "suggestion-app-firebase.appspot.com",
   messagingSenderId: "916677202302",
   appId: "1:916677202302:web:59acb081df11457e3e632f",
-}; */
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
